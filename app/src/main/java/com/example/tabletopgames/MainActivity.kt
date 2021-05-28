20dadd834a9f6c67d03bd6b6e6bf993fb82d268c
ajaxgeneral <45013052+ajaxgeneral@users.noreply.github.com>
2021-05-28 08:16:48 -0500

    Intial project setup and creation of the splash, main, and reservation activities with navigation between them.

---
+++ app/src/main/java/com/example/tabletopgames/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.tabletopgames
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun alert(view: View) {
+        Toast.makeText(this,"Coming Soon", Toast.LENGTH_SHORT).show()
+    }
+
+    fun reservations(view: View){
+        val startReservations = Intent(this, ReservationActivity::class.java)
+        startActivity(startReservations)
+    }
+}
