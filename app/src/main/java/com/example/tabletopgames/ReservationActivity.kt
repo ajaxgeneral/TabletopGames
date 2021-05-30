27e7f0bf98d1753516b57df98b0a5709399a050e
ajaxgeneral <45013052+ajaxgeneral@users.noreply.github.com>
2021-05-30 13:07:01 -0500

    v2 Reservations Avtivity with mock data

--- app/src/main/java/com/example/tabletopgames/ReservationActivity.kt
+++ app/src/main/java/com/example/tabletopgames/ReservationActivity.kt
@@ -1,20 +1,38 @@
 package com.example.tabletopgames
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ReservationActivity : AppCompatActivity() {
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<ReservationsRecyclerAdapter.ViewHolder>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        var text = "\t\t\t\t\t\t\t\t\t\tComing Soon," +
+                "\n\t\t\t\t\t\t\t\t\tAdd a Reservation!"
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservation)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        layoutManager = LinearLayoutManager(this)
+        val recyclerView = findViewById<RecyclerView>(R.id.reservations_recyclerView)
+        recyclerView.layoutManager = layoutManager
+
+        adapter = ReservationsRecyclerAdapter()
+        recyclerView.adapter = adapter
+
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, text,Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
     }
 }
